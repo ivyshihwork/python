d9385c1e3aac0eeb4c9790b1a88545d40a62d120
Ivy Shih <ivy.shih.work@gmail.com>
2021-05-17 12:08:34 -0700

    Completed player and movement functions.

--- 05_TurtleCrossing/main.py
+++ 05_TurtleCrossing/main.py
@@ -4,11 +4,23 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+# Setting up screen
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Ivy Shih's Turtle Crossing Capstone Project")
 screen.tracer(0)
 
+#Setting up Player
+player = Player()
+
+screen.listen()
+screen.onkeypress(player.up, "Up")
+screen.onkeypress(player.down, "Down")
+screen.onkeypress(player.left, "Left")
+screen.onkeypress(player.right, "Right")
+
 game_is_on = True
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
--- 05_TurtleCrossing/player.py
+++ 05_TurtleCrossing/player.py
@@ -1,7 +1,26 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.shape("turtle")
+        self.setheading(90)
+        self.penup()
+        self.goto(STARTING_POSITION)
+
+    def up(self):
+        self.goto(self.xcor(), self.ycor() + MOVE_DISTANCE)
+
+    def down(self):
+        self.goto(self.xcor(), self.ycor() - MOVE_DISTANCE)
+
+    def left(self):
+        self.goto(self.xcor() - MOVE_DISTANCE, self.ycor())
+
+    def right(self):
+        self.goto(self.xcor() + MOVE_DISTANCE, self.ycor())

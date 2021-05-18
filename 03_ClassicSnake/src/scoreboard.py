c4d409fb3ade24e54f26de37da2de45a6ebf67c7
Ivy Shih <ivy.shih.work@gmail.com>
2021-05-18 14:14:52 -0700

    Add feature to write high score to file

--- 03_ClassicSnake/src/scoreboard.py
+++ 03_ClassicSnake/src/scoreboard.py
@@ -6,7 +6,7 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.loadHighScore()
         super().__init__()
         self.penup()
         self.color('white')
@@ -21,8 +21,21 @@ class Scoreboard(Turtle):
     def resetScoreboard(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.writeHighScore()
         self.update()
         self.score = 0
+
+    def loadHighScore(self):
+        try:
+            with open("highscore.txt") as file:
+                #self.highscore = int(file.read().split(' ')[0])
+                return int(file.read())
+        except (OSError, IOError):
+                return 0
+
+    def writeHighScore(self):
+        with open("highscore.txt", mode="w+") as file:
+            file.write(str(self.high_score))
 
     def update(self):
         self.clear()

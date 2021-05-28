93fb05647831a94c9a925362d702ba9507d918fc
Ivy Shih <ivy.shih.work@gmail.com>
2021-05-27 17:18:14 -0700

    add functionality to look up state and enter state name on map

--- 06_us-states-game/README.md
+++ 06_us-states-game/README.md
@@ -1,10 +1,13 @@
 US States Guessing Game
 =======================
 
-(In-progress, currently blocked due to fixing issues with system library update)
+(In-progress)
 
 This game has an interactive map.
-When the center of the state is clicked, a pop-up window will be displayed asking for the state name.
+When the game starts, a pop-up window will be displayed asking for the state name.
+Enter a state name and click ok.  
+
+If a state name is guessed correctly, the name will be displayed on the map.
 
 This is written based on Turtle ad Pandas library.
 
--- 06_us-states-game/main.py
+++ 06_us-states-game/main.py
@@ -1,13 +1,50 @@
 from turtle import Turtle, Screen
 import os
+import pandas
+import csv
 
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'blank_states_img.gif')
+img_file = os.path.join(dirname, 'blank_states_img.gif')
+state_file = os.path.join(dirname, '50_states.csv')
 
 screen = Screen()
 screen.title("US States Game")
-screen.bgpic(filename)
-#screen.addshape(filename)
+screen.bgpic(img_file)
 
-screen.listen()
-screen.exitonclick()
+TOTAL = 50
+right_answer = 0
+right_guesses = []
+
+state_coors = pandas.read_csv(state_file)
+
+state_names = [ state.lower() for state in state_coors.state.to_list() ]
+
+def writeState(state,x, y):
+    name=Turtle()
+    name.hideturtle()
+    name.color("black")
+    name.penup()
+    name.goto(x, y)
+    name.write(f"{state}", False, 'center',  ("Courier", 12, "normal"))
+
+def get_mouse_click_coor(x,y):
+    print(x,y)
+
+answer = screen.textinput(title="Guess the state", prompt="Enter the name of the state: ").lower()
+
+while len(right_guesses) < 50:
+
+    new_title = f"{right_answer}/{TOTAL} Guessed"
+
+
+    if answer in state_names:
+        right_answer += 1
+        new_title = f"{right_answer}/{TOTAL} has been quessed."
+        state_info = state_coors[state_coors.state.str.lower() == answer]
+        state_name = state_info.state
+        state_x = int(state_info.x)
+        state_y = int(state_info.y)
+        right_guesses.append(state_name)
+        writeState(state_name, state_x, state_y)
+
+    answer = screen.textinput(title=new_title, prompt="Enter another state name: ").lower()

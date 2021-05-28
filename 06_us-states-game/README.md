90e40880ee8a249553c96bf5737efa1e5ee4c860
Ivy Shih <ivy.shih.work@gmail.com>
2021-05-27 21:20:26 -0700

    add feature to generate a list of names in csv files for state names not guessed.

--- 06_us-states-game/README.md
+++ 06_us-states-game/README.md
@@ -1,13 +1,14 @@
-US States Guessing Game
-=======================
+US State Name Guessing Game
+===========================
 
-(In-progress)
-
-This game has an interactive map.
-When the game starts, a pop-up window will be displayed asking for the state name.
+When the game starts, a pop-up window will be displayed asking for any US state name.
 Enter a state name and click ok.  
 
 If a state name is guessed correctly, the name will be displayed on the map.
+
+If that's all player can remember, enter 'exit' in the box, and the game will exit.
+A CSV file called "states_to_learn.csv" will be created.
+It will contain the rest of the US states that were not guessed correctly.
 
 This is written based on Turtle ad Pandas library.
 
--- 06_us-states-game/main.py
+++ 06_us-states-game/main.py
@@ -6,9 +6,10 @@ import csv
 dirname = os.path.dirname(__file__)
 img_file = os.path.join(dirname, 'blank_states_img.gif')
 state_file = os.path.join(dirname, '50_states.csv')
+states_to_learn = os.path.join(dirname, 'states_to_learn.csv')
 
 screen = Screen()
-screen.title("US States Game")
+screen.title("US State Name Guessing Game")
 screen.bgpic(img_file)
 
 TOTAL = 50
@@ -17,34 +18,36 @@ right_guesses = []
 
 state_coors = pandas.read_csv(state_file)
 
-state_names = [ state.lower() for state in state_coors.state.to_list() ]
+state_names =  state_coors.state.to_list()
 
 def writeState(state,x, y):
     name=Turtle()
-    name.hideturtle()
     name.color("black")
+    name.hideturtle()
     name.penup()
     name.goto(x, y)
-    name.write(f"{state}", False, 'center',  ("Courier", 12, "normal"))
-
-def get_mouse_click_coor(x,y):
-    print(x,y)
-
-answer = screen.textinput(title="Guess the state", prompt="Enter the name of the state: ").lower()
+    name.write(f"{state}", False, 'center',  ("Courier", 10, "normal"))
 
 while len(right_guesses) < 50:
 
     new_title = f"{right_answer}/{TOTAL} Guessed"
-
+    answer = screen.textinput(title=new_title, prompt="Enter another state name: ").title()
 
     if answer in state_names:
         right_answer += 1
         new_title = f"{right_answer}/{TOTAL} has been quessed."
-        state_info = state_coors[state_coors.state.str.lower() == answer]
-        state_name = state_info.state
+        state_info = state_coors[state_coors.state == answer]
+        state_name = state_info.state.values[0]
         state_x = int(state_info.x)
         state_y = int(state_info.y)
         right_guesses.append(state_name)
         writeState(state_name, state_x, state_y)
+    elif answer == 'Exit':
+        break
 
-    answer = screen.textinput(title=new_title, prompt="Enter another state name: ").lower()
+# generate a file of missing states upon exit
+states_to_learn_list =  list(set(state_names) - set(right_guesses))
+states_to_learn_list.sort()
+print(states_to_learn_list)
+df = pandas.DataFrame(states_to_learn_list, columns=['state'])
+df.to_csv(states_to_learn)

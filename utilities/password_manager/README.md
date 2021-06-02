28443605fc2e1b596b42f837d3970a04ae88ed72
Ivy Shih <ivy.shih.work@gmail.com>
2021-06-02 02:23:58 -0700

    add profile update feature to password manager

--- utilities/password_manager/README.md
+++ utilities/password_manager/README.md
@@ -3,9 +3,12 @@ Password Manager
 
 This password manager has a graphical UI to keep track and generate password for different websites.
 
-The information will be stored in password_info.txt locally to this python program.
+The information will be stored in password_info.json locally to this python program.
 
 Once 'Generate password' button is clicked, it will also copy the password to the clipboard to be used.
+
+Search function is based on the website name and is case insensitive.
+Once a record on file is found, all the fields will be updated to that record.
 
 Dependencies:  pypercip, tkinter
 
--- utilities/password_manager/main.py
+++ utilities/password_manager/main.py
@@ -31,28 +31,21 @@ def pw_gen():
 # Trigger by Add button, separate data fields by |
 def save_info():
     website_info = website_entry.get()
+    url_info = url_entry.get()
     id_info = id_entry.get()
     password_info = password_entry.get()
-    #new_entry = website_info + ' | ' + id_info + ' | ' + password_info + '\n'
+
     new_entry = {
         website_info.lower():{
+            'url': url_info,
             'id': id_info,
-            'Password': password_info,
+            'password': password_info,
         }
     }
 
-    if len(website_info) == 0 or len(id_info) == 0 or len(password_info) == 0:
+    if len(website_info) == 0 or len(url_info) ==0 or len(id_info) == 0 or len(password_info) == 0:
         messagebox.showerror(title='Error', message="Do not leave any field empty!")
     else:
-        # ok_to_save = messagebox.askokcancel(title='Confirmation', message=f"New entry has:\n\n\tWebsite={website_info}\n\tEmail={id_info}\n\tPassword={password_info}\n\nIs it okay to save?\n")
-        # if ok_to_save == True:
-        #     with open(data_file, 'w+') as df:
-        #         df.write(new_entry)
-        #     website_entry.delete(0,tkinter.END)
-        #     password_entry.delete(0, tkinter.END)
-        #     messagebox.showinfo(title="Success", message="A entry has been added to password_info.txt." )
-        # else:
-        #     messagebox.showinfo(title="Action Cancelled", message="Data is not saved.\n")
         try:
             df = open (data_file, 'r')
             data = json.load(df)
@@ -65,12 +58,38 @@ def save_info():
             with open(data_file, 'w') as df2:
                 json.dump(data, df2, indent=4)
 
-        messagebox.showinfo(title="Success", message="A entry has been added to password_info.txt." )
+        messagebox.showinfo(title="Success", message=f"A entry has been added/updated to:\n{data_file}." )
         website_entry.delete(0,tkinter.END)
+        url_entry.delete(0,tkinter.END)
         id_entry.delete(0,tkinter.END)
         password_entry.delete(0, tkinter.END)
 
 # ---------------------------- Search --------------------------------- #
+
+def search():
+    website_info = website_entry.get()
+
+    try:
+        with open (data_file, 'r') as df:
+            data = json.load(df)
+    except (FileNotFoundError, NameError,):
+        messagebox.showinfo(title="Warning", message="No data file is found.\nPlease add some data first." )
+    except (json.decoder.JSONDecodeError):
+        messagebox.showinfo(title="Warning", message="No website has been entered on file." )
+    else:
+        if website_info.lower() in data:
+            url_info = data[website_info.lower()]['url']
+            id_info = data[website_info.lower()]['id']
+            password_info = data[website_info.lower()]['password']
+            id_entry.delete(0,tkinter.END)
+            url_entry.delete(0,tkinter.END)
+            password_entry.delete(0, tkinter.END)
+            id_entry.insert(0, id_info)
+            url_entry.insert(0, url_info)
+            password_entry.insert(0, password_info)
+        else: # if 'id_info' not in locals():
+            messagebox.showinfo(title="Warning", message=f"No record exists for entered website." )
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -86,35 +105,39 @@ canvas.grid(column=1, row=0)
 
 # fields - label and more
 
-website_label = tkinter.Label(text="Website:")
+website_label = tkinter.Label(text="Website Name:")
 website_label.grid(column=0, row=1)
 
-website_entry = tkinter.Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
-# website_entry = tkinter.Entry(width=21)
-# website_entry.grid(column=1, row=1)
+website_entry = tkinter.Entry(width=21)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
-# search_button = tkinter.Button(text='Search', width=14)
-# search_button.grid(column=2, row=1)
+search_button = tkinter.Button(text='Search', width=14, command=search)
+search_button.grid(column=2, row=1)
 
-id_label = tkinter.Label(text="Email/Username:")
-id_label.grid(column=0,row=2)
+url_label = tkinter.Label(text="Website URL:")
+url_label.grid(column=0, row=2)
+
+url_entry = tkinter.Entry(width=35)
+url_entry.grid(column=1,row=2, columnspan=2)
+
+id_label = tkinter.Label(text="Username/Email:")
+id_label.grid(column=0,row=3)
 
 id_entry = tkinter.Entry(width=35)
-id_entry.grid(column=1, row=2, columnspan=2)
+id_entry.grid(column=1, row=3, columnspan=2)
 
 password_label = tkinter.Label(text='Password:')
-password_label.grid(column=0, row=3)
+password_label.grid(column=0, row=4)
 
 password_entry = tkinter.Entry(width=21)
-password_entry.grid(column=1, row=3)
+password_entry.grid(column=1, row=4)
 
 password_button = tkinter.Button(text='Generate Password', command=pw_gen)
-password_button.grid(column=2, row=3)
+password_button.grid(column=2, row=4)
 
-add_button = tkinter.Button(text='Add',width=36, command=save_info)
-add_button.grid(column=1, row=4,columnspan=2)
+add_button = tkinter.Button(text='Add/Update',width=36, command=save_info)
+add_button.grid(column=1, row=5,columnspan=2)
 
 
 window.mainloop()

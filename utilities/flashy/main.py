27b2ea2961b0016bc329d8129f45f6aeb2750791
Ivy Shih <ivy.shih.work@gmail.com>
2021-06-02 22:55:31 -0700

    flashy: final clean up

--- utilities/flashy/main.py
+++ utilities/flashy/main.py
@@ -14,8 +14,8 @@ BACKGROUND_COLOR = "#B1DDC6"
 dirname = os.path.dirname(__file__)
 card_front_img = os.path.join(dirname, 'images/card_front.png')
 card_back_img = os.path.join(dirname, 'images/card_back.png')
-check_img = os.path.join(dirname, 'images/right.png')
-x_img = os.path.join(dirname, 'images/wrong.png')
+right_img = os.path.join(dirname, 'images/right.png')
+wrong_img = os.path.join(dirname, 'images/wrong.png')
 data_file = os.path.join(dirname, 'data/french_words.csv')
 to_learn_file = os.path.join(dirname, 'data/words_to_learn.csv')
 
@@ -28,10 +28,10 @@ except (FileNotFoundError):
         all_data = pandas.read_csv(df)
 finally:
     xlate_dict = all_data.to_dict()
-
     lang= list(xlate_dict.keys())[0]
     xlate = list(xlate_dict.keys())[1]
     xlate_dict = all_data.to_dict(orient='records') # [{'French': 'partie', 'English': 'part'},
+    print(len(xlate_dict))
 
 
 #------------------------ Generate random word --------------------------
@@ -81,12 +81,12 @@ lang_text = canvas.create_text(400, 150, text=lang, fill='black', font=('Ariel',
 random_word = random.choice(xlate_dict)
 lang_word = canvas.create_text(400, 263, text=random_word[lang], fill='Black', font=('Ariel', 60, 'bold'))
 
-w_img = tkinter.PhotoImage(file=x_img)
-wrong_button = tkinter.Button(image=w_img, highlightthickness=0, command=next_card)
+x_img = tkinter.PhotoImage(file=wrong_img)
+wrong_button = tkinter.Button(image=x_img, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
-r_img = tkinter.PhotoImage(file=check_img)
-right_button = tkinter.Button(image=r_img, highlightthickness=0, command=remove_and_next)
+check_img = tkinter.PhotoImage(file=right_img)
+right_button = tkinter.Button(image=check_img, highlightthickness=0, command=remove_and_next)
 right_button.grid(column=1, row=1)
 
 

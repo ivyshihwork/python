845f50bc6721062a5ed5d064c93e4f29a42a7f8a
Ivy Shih <ivy.shih.work@gmail.com>
2021-06-02 22:12:15 -0700

    flashy: add card flipping function

--- utilities/flashy/main.py
+++ utilities/flashy/main.py
@@ -17,6 +17,7 @@ card_back_img = os.path.join(dirname, 'images/card_back.png')
 right_img = os.path.join(dirname, 'images/right.png')
 wrong_img = os.path.join(dirname, 'images/wrong.png')
 data_file = os.path.join(dirname, 'data/french_words.csv')
+SIDE = 0
 
 #------------------------ Read Data --------------------------
 try:
@@ -30,16 +31,28 @@ else:
     lang= list(xlate_dict.keys())[0]
     xlate = list(xlate_dict.keys())[1]
 
-    # lang_words = all_data[lang].tolist()
-    # xlate_words = all_data[xlate].tolist()
+    xlate_dict = all_data.to_dict(orient='records') # [{'French': 'partie', 'English': 'part'},
 
 
 #------------------------ Generate random word --------------------------
-def generate_lang_word():
-    global all_data,lang, xlate
-    random_lang = all_data[lang].tolist()[random.randint(0, len(all_data[xlate].tolist())-1 )]
-    
-    canvas.itemconfig(lang_word,text=random_lang)
+def next_card():
+    global all_data,lang, xlate, random_word, flip_timer
+    random_word = random.choice(xlate_dict)
+    canvas.itemconfig(lang_word,text=random_word[lang])
+    # Flip card to front
+    window.after_cancel(flip_timer)
+    canvas.itemconfig(card_side, image=front_img)
+    canvas.itemconfig(lang_text, text=lang, fill='black')
+    canvas.itemconfig(lang_word, text=random_word[lang], fill='black')
+    flip_timer = window.after(3000, flip_card)
+
+#------------------------ flip card ---------------------------
+
+def flip_card():
+    # Flip card to the back
+    canvas.itemconfig(card_side, image=back_img)
+    canvas.itemconfig(lang_text, text=xlate, fill='white')
+    canvas.itemconfig(lang_word, text=random_word[xlate], fill='white')
 
 
 #------------------------ UI Setup ---------------------------
@@ -47,23 +60,25 @@ def generate_lang_word():
 window = tkinter.Tk()
 window.title('Flashy')
 window.config(padx=20,pady=20, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, flip_card)
 
 front_img = tkinter.PhotoImage(file=card_front_img)
+back_img = tkinter.PhotoImage(file=card_back_img)
 canvas = tkinter.Canvas(width=800, height=526, bg=BACKGROUND_COLOR ,highlightthickness=0)
-canvas.create_image(400, 263, image=front_img)
+card_side = canvas.create_image(400, 263, image=front_img)
 canvas.grid(column=0, row=0, columnspan=2)
 
 lang_text = canvas.create_text(400, 150, text=lang, fill='black', font=('Ariel', 40, 'italic'))
 
-new_rand = all_data[lang].tolist()[random.randint(0, len(all_data[xlate].tolist())-1 )]
-lang_word = canvas.create_text(400, 263, text=new_rand, fill='Black', font=('Ariel', 60, 'bold'))
+random_word = random.choice(xlate_dict)
+lang_word = canvas.create_text(400, 263, text=random_word[lang], fill='Black', font=('Ariel', 60, 'bold'))
 
 w_img = tkinter.PhotoImage(file=wrong_img)
-wrong_button = tkinter.Button(image=w_img, highlightthickness=0, command=generate_lang_word)
+wrong_button = tkinter.Button(image=w_img, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 r_img = tkinter.PhotoImage(file=right_img)
-right_button = tkinter.Button(image=r_img, highlightthickness=0, command=generate_lang_word)
+right_button = tkinter.Button(image=r_img, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 
 

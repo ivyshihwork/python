8f7cfecd789a4230355f7159dae80ea2dcad08e8
Ivy Shih <ivy.shih.work@gmail.com>
2021-06-08 15:02:14 -0700

    Clean up the file for easy read.

--- fun/trivia/ui.py
+++ fun/trivia/ui.py
@@ -7,22 +7,25 @@ dirname = os.path.dirname(__file__)
 right = os.path.join(dirname, 'images/true.png')
 wrong = os.path.join(dirname, 'images/false.png')
 
+
 class QuizInterface:
     def __init__(self, qb: quiz_brain.QuizBrain):
         self.quizbrain = qb
         self.window = tkinter.Tk()
         self.window.title('10 Trivia Questions')
-        self.window.config(padx=20,pady=20, bg="#375362")
+        self.window.config(padx=20, pady=20, bg="#375362")
 
-        self.score_txt = tkinter.Label(text=f'Score: {self.quizbrain.score}/{self.quizbrain.question_number+1}', fg='white', bg=THEME_COLOR)
+        self.score_txt = tkinter.Label(text=f'Score: {self.quizbrain.score}/{self.quizbrain.question_number + 1}',
+                                       fg='white', bg=THEME_COLOR)
         self.score_txt.grid(column=1, row=0)
 
-        self.canvas = tkinter.Canvas(width=300, height=250, bg='white' ,highlightthickness=0)
-        self.question = self.canvas.create_text(150, 125, width=280, text="", fill=THEME_COLOR, font=('Arial', 25, 'italic'))
-        self.canvas.grid(column=0, row=1, columnspan=2,pady=50)
+        self.canvas = tkinter.Canvas(width=300, height=250, bg='white', highlightthickness=0)
+        self.question = self.canvas.create_text(150, 125, width=280, text="", fill=THEME_COLOR,
+                                                font=('Arial', 25, 'italic'))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         self.true_img = tkinter.PhotoImage(file=right)
-        self.true_button = tkinter.Button(image=self.true_img,highlightthickness=0, command=self.true_clicked)
+        self.true_button = tkinter.Button(image=self.true_img, highlightthickness=0, command=self.true_clicked)
         self.true_button.grid(column=0, row=3)
 
         self.false_img = tkinter.PhotoImage(file=wrong)
